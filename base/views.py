8f6ef26f541db688091c55cc9531935fe5ba58d8
Ananth Dayanithi <anantha.e0420003@sret.edu.in>
2024-11-25 15:12:57 +0530

    User Registeration

--- base/views.py
+++ base/views.py
@@ -1,10 +1,14 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Task
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView,FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect  # Add this import
+
 class CustomLoginView(LoginView):
     template_name= 'base/login.html'
     model =  Task
@@ -27,11 +31,29 @@ class TaskList(LoginRequiredMixin, ListView):
         context['task_count'] = context['tasks'].filter(complex=False).count()
         return context
 
-    
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('task-list')  # Redirect to task list after successful registration
+
+    def form_valid(self, form):
+        # Save the user and log them in automatically
+        user = form.save()
+        if user:
+            login(self.request, user)  # Log the user in
+        return super().form_valid(form)
+
+    def get(self, *args, **kwargs):
+        # If the user is already authenticated, redirect them to the task list
+        if self.request.user.is_authenticated:
+            return redirect('task-list')
+        return super().get(*args, **kwargs)
 
 class TaskDetail(LoginRequiredMixin,DetailView):
     model = Task
     context_object_name = 'task'
+    success_url = reverse_lazy('task-list')
     template_name = 'base/task.html'
 
 class TaskCreate(LoginRequiredMixin, CreateView):
